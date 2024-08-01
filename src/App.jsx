36523cebcac92a4f62a9278171f901589081dd58
el3c_tron <aniketkumar28032002@gmail.com>
2024-08-01 19:25:32 +0530

    component transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react"
-import { Home, Loading, Skills_NRM } from "./components"
+import { Home, Loading, Skills_NRM, Skills_TML } from "./components"
 import { Board } from "./components/Svgs"
 
 function App() {
 
   const [loading ,setLoading] = useState(false);
+  const [overview, setOverview] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 6500)
-  }, [])
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 6500)
+  // }, [])
 
   return (loading) ? (<Loading />) : (
     <>
       <Home />
-      <Skills_NRM />
+      {
+        (overview) ? (<Skills_NRM setOverview = {setOverview} />) : (<Skills_TML setOverview = {setOverview} />)
+      }
     </>
   )
 }
