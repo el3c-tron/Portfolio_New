c23603339ff147b35e77f67e561c745c650dd497
el3c_tron <aniketkumar2832002@gmail.com>
2024-08-16 21:20:44 +0530

    debugging

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -9,17 +9,7 @@ const tags = ["ReactJS", "MongoDB", "mongoose", "NodeJS", "ExpressJS", "Javascri
 function Test() {
   return (
     <>
-        <div className=' h-screen w-full flex justify-center items-center'>
-            <div className='w-[40%] flex items-center h-fit overflow-hidden p-2'>
-                <Marquee className='p-2'>
-                    {
-                        tags.map((tag) => (
-                            <span className='tags pl-2 pr-2 pt-1 pb-1 ml-4 bg-gray-200 rounded-md flex-shrink-0'>{tag}</span>
-                        ))
-                    }
-                </Marquee>
-            </div>
-        </div>
+        
     </>
   )
 }
